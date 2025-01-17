de4d280c45bbc6c3f77d2aecbfdf5ec3e0788c93
mvelez88 <mvelezg88@gmail.com>
2025-01-17 16:50:44 -0500

    Player Model and Animation - Idle, Walk

--- Propuesta nivel 1/Assets/MVG/Scripts/Player/PlayerMovement.cs
+++ Propuesta nivel 1/Assets/MVG/Scripts/Player/PlayerMovement.cs
@@ -6,10 +6,30 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f; // Velocidad de movimiento del Jugador
 
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
+        bool isWalking = animator.GetBool("isWalking");
+        bool movePressed = Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") !=0 ;
+
         float moveHorizontal = Input.GetAxis("Horizontal"); // Mover el jugador de forma horizontal con teclas lados o A y D
         float moveVertical = Input.GetAxis("Vertical"); // Mover el jugador de forma Vetical con teclas arriba y abajo o W y S
+
+        if (!isWalking && movePressed)
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        if (isWalking && !movePressed)
+        {
+            animator.SetBool("isWalking", false);
+        }
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); // Nuevo vector movement para aplicar el movimiento al Jugador
         transform.Translate(movement * speed * Time.deltaTime); // Se le aplica el movimiento al jugador en la dirección de las teclas y con la velocidad establecida
